35abf0276a24e8e7f403ec018c7f05829f94d856
KSDragon82 <www.kiarashs2929@gmail.com>
2020-08-23 12:54:23 +0430

    deploying to heroku

--- btre/settings.py
+++ btre/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z2ipq#*pk8cq(t%(!#y9c95z=!$3u%=-cncli1m+-es%adzo_e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['kiarash-portfolio.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['btre-realestate-82.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
